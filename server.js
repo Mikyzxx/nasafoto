1d1f92e8b9fe77192659c653c1283558f140977b
Miguel Cardenas <mikyzx054@gmail.com>
2025-04-04 14:08:28 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const NASA_API_KEY = process.env.NASA_API_KEY || "APY_KEY"; // API Key aqui
+
+app.use(cors()); // Para permitir solicitudes desde el frontend
+app.use(express.json());
+app.use(express.static("public"));
+
+// obtener la imagen del día (APOD)
+app.get("/apod", async (req, res) => {
+    try {
+        const response = await axios.get(
+            `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`
+        );
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: "Error al obtener datos de la NASA" });
+    }
+});
+
+// Ruta principal que muestra el HTML
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
