4e0b20e1178c6b8f8a21172c6a620fa4eec2ff82
Miguel Cardenas <mikyzx054@gmail.com>
2025-04-04 14:22:33 -0700

    linea 28 a 30 omentada

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.get("/apod", async (req, res) => {
 });
 
 // Ruta principal que muestra el HTML
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
